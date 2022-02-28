5583ff61d124e429b13bdfedb67eddf305645912
adeem <adeemaq@gmail.com>
2022-02-28 13:50:28 +1100

    first commit

---
+++ source.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+
+int main(){
+
+    std::cout << "Hello world\n";
+
+    std::cout << "Nice to meet " << "you\n";
+
+    //types
+    char c = 'a';
+    int x = 123 ;
+     double d = 3.14;
+    std::cout << "the character is" << c << "\n";
+    std::cout << "The integer is" << x << "\n";
+    std::cout << "The value of d is: " << d << "\n";
+}
